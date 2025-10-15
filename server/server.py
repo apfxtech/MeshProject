7ff7728b6a2b925cd0ce0b1750b0bf3e42524be9
apfxtech <aperturefoxtechnology@gmail.com>
2025-10-15 16:50:06 +0300

    разметка программы и получение разметки пакетов

--- server/server.py
+++ server/server.py
@@ -1,30 +1,56 @@
+import asyncio
 import meshtastic
+from meshtastic.serial_interface import SerialInterface
 from pubsub import pub
 import logging
+import sys
 
 logging.root.setLevel(logging.NOTSET)
 logging.basicConfig(
-    level="DEBUG",
+    level=logging.DEBUG,
     format="%(asctime)s %(name)s [%(levelname)s] %(message)s",
     datefmt='%d-%b-%y %H:%M:%S',
     force=True,
-    handlers=[
-        logging.FileHandler("server.log")
-    ]
+    handlers=[logging.FileHandler("server.log")]
 )
 
 logger = logging.getLogger("main")
-client = None
 
-def onReceive(packet, client):
-    logger.debug(f"Received: {packet}")
+class MainApp:
+    def __init__(self):
+        pub.subscribe(self.onClose, "meshtastic.connection.lost")
+        pub.subscribe(self.onConnection, "meshtastic.connection.established")
+        pub.subscribe(self.onReceivePacket, "meshtastic.receive")
+        pub.subscribe(self.onReceiveText, "meshtastic.receive.text")
+        
+        ports = meshtastic.util.findPorts()
+        if ports <= 0: self.close()
+        self.ports, port = ports, ports[0]
+        self.client = SerialInterface(devPath=port)
 
-def onConnection(client, topic=pub.AUTO_TOPIC): 
-    client.sendText("hello from python")
+    def onReceivePacket(self, packet, interface):
+        logger.debug(f"Получен пакет: {packet}")
+
+    def onReceiveText(self, packet, interface):
+        logger.debug(f"Получено сообщение: {packet}")
+    
+    def onConnection(self, client, topic=pub.AUTO_TOPIC):
+        logger.info(f"Соединение установлено")
+
+    def onClose(self, client, topic=pub.AUTO_TOPIC):
+        logger.warning(f"Соединение потерянно")
+        self.close()
+
+    def close(self):
+        logger.warning(f"Закрытие программы")
+        sys.exit(1)
+
+async def main():
+    app = MainApp()
+    while True: await asyncio.sleep(10)
 
 if __name__ == "__main__":
-    serial_ports = meshtastic.util.findPorts()
-    client = meshtastic.serial_client.Serialclient()
-    pub.subscribe(onReceive, "meshtastic.receive")
-    pub.subscribe(onConnection, "meshtastic.connection.established")
-
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.warning("Завершение программы пользователем.")
